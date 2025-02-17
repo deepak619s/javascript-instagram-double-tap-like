dc9be40d4223288c5d92aa935704c8f5af0cb4db
Deepak <sinhadeepak619@gmail.com>
2025-02-17 14:53:02 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const clickCount = document.querySelector('.clickCount');
+const clickArea = document.querySelector('.inner-div');
+
+let likeCount = 0;
+
+const showHeart = (e) => {
+    const heart = document.createElement('i');
+    heart.classList.add('fas');
+    heart.classList.add('fa-heart');
+    clickArea.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 800);
+}
+
+clickArea.addEventListener('dblclick', (e) => {
+    clickCount.innerHTML = ++likeCount;
+    showHeart(e);
+})
